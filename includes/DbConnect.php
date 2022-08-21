1cd3246ee1868ab38acc13db7ed7a41180da8a61
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-21 09:16:53 -0600

    update

--- includes/DbConnect.php
+++ includes/DbConnect.php
@@ -13,8 +13,8 @@ class DbConnect
         $conn = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
         // Check for database connection error
-        if (mysqli_connect_errno()) {
-            echo "Failed to connect to MySQL: " . mysqli_connect_error();
+        if ($conn->connect_error) {
+            echo "Failed to connect to MySQL: " . $conn->connect_error;
         }
         echo "Success";
 
