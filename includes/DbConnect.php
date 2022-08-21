56e0862fbddd76d53f88de0f8cae50a112214b3e
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-21 00:16:16 -0600

    update

--- includes/DbConnect.php
+++ includes/DbConnect.php
@@ -23,6 +23,7 @@ class DbConnect
         if (mysqli_connect_errno()) {
             echo "Failed to connect to MySQL: " . mysqli_connect_error();
         }
+        echo "Success";
 
         // returing connection resource
         return $this->conn;
