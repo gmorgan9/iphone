5839b562d9361278c3880e102933157213ca60cb
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-21 00:35:47 -0600

    update

---
+++ api/database.php
@@ -0,0 +1,27 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "BIGmorgan1999!";
+$dbname = "iphone";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+//getting values
+$teamName = $_POST['name'];
+$memberCount = $_POST['member'];
+
+$sql = "INSERT INTO team(name, member) values('$teamName', '$memberCount')";
+
+if (mysqli_query($conn, $sql)) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
