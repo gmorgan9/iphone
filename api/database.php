768ae330c35fb5c5a7c2ea38b57241d7fffb43fb
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-21 00:36:28 -0600

    update

--- api/database.php
+++ api/database.php
@@ -10,6 +10,7 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
+echo "Success";
 
 //getting values
 $teamName = $_POST['name'];
