e8f430016a95d2f407f57aecbf6297a0c21fed05
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-21 09:05:00 -0600

    update

--- includes/Config.php
+++ includes/Config.php
@@ -1,6 +1,20 @@
-<?php
-
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'BIGmorgan1999!');
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'iphone');
+<?php 
+    class Database {
+        private $host = "127.0.0.1";
+        private $database_name = "iphone";
+        private $username = "root";
+        private $password = "BIGmorgan1999!";
+        public $conn;
+        public function getConnection(){
+            $this->conn = null;
+            try{
+                $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->database_name, $this->username, $this->password);
+                $this->conn->exec("set names utf8");
+            }catch(PDOException $exception){
+                echo "Database could not be connected: " . $exception->getMessage();
+            }
+            echo "Success";
+            return $this->conn;
+        }
+    }  
+?>
